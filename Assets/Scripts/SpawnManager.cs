c355340dcddb8950c97d27cfd3c065cb07d64dac
xachan <xachan@s.sfusd.edu>
2023-02-17 11:30:57 -0800

    End of 3.1

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,21 +6,27 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
-    private float startDelay = 2;  
+    private float startDelay = 2;
     private float repeatRate = 2;
-   
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
 
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
+
