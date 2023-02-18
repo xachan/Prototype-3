5a127de4fc10b8691efb7f4318aeea3e019ae557
xachan <xachan@s.sfusd.edu>
2023-02-17 16:12:06 -0800

    finish challenge 3

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -6,6 +6,8 @@ public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
     private float repeatWidth;
+   
+
     // Start is called before the first frame update
     void Start()
     {
